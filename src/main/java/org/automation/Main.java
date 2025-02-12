6eabff88ec75d3de98ec92312c9052e0d975bbe9
sowrabhav <sowphotos@gmail.com>
2025-02-11 18:42:59 -0600

    update new libraries

--- src/main/java/org/automation/Main.java
+++ src/main/java/org/automation/Main.java
@@ -1,4 +1,9 @@
 package org.automation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,10 +13,12 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+            driver.get("https://www.google.com");
+            WebElement element = driver.findElement(By.name("q"));
+            element.sendKeys("Tajmahal");
+//        element.submit();
+//        driver.quit();
         }
     }
-}

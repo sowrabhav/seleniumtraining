4918117e83e835aed35332497201ad7886132cb1
Baba Fakruddin D <bfakruddin@gmail.com>
2025-02-13 23:05:29 +0530

    Java - datatypes and wrapperclasses

--- src/main/java/org/javapractice/DataTypes.java
+++ src/main/java/org/javapractice/DataTypes.java
@@ -1,4 +1,86 @@
 package org.javapractice;
 
 public class DataTypes {
+    // Data type is a classification of data
+    // Data type is a classification of variables
+    // Data type is a classification of constants
+    // Data type is a classification of data into different categories
+    // Data type is a classification of data into different types
+    // Data types are used to define the type of data that can be stored in a variable
+
+    // Data types in Java are int, float, double, char, boolean, byte, short, long
+
+    // int data type
+    // int data type is used to store integer values
+    // int data type is used to store whole numbers
+    // int data type is used to store positive and negative numbers
+    // int data type is used to store numbers without decimal points
+    // int data type is used to store numbers in the range of -2,147,483,648 to 2,147,483,647
+
+    // int data type example
+    int age = 30;
+
+    // float data type
+    // float data type is used to store floating-point values
+    // float data type is used to store numbers with decimal points
+    // float data type is used to store numbers with single precision
+    // float data type is used to store numbers in the range of 3.4e-038 to 3.4e+038
+
+    // float data type example
+    float price = 10.5f;
+
+    // double data type
+    // double data type is used to store floating-point values
+    // double data type is used to store numbers with decimal points
+    // double data type is used to store numbers with double precision
+    // double data type is used to store numbers in the range of 1.7e-308 to 1.7e+308
+
+    // double data type example
+    double amount = 100.75;
+
+    // char data type
+    // char data type is used to store characters
+    // char data type is used to store single characters
+    // char data type is used to store alphabets, digits, and special characters
+    // char data type is used to store characters in the range of 0 to 65,535
+
+    // char data type example
+    char c = 'A';
+
+    // boolean data type
+    // boolean data type is used to store boolean values
+    // boolean data type is used to store true or false values
+    // boolean data type is used to store logical values
+    // boolean data type is used to store only two values: true or false
+
+    // boolean data type example
+    boolean isTrue = true;
+
+    // byte data type
+    // byte data type is used to store integer values
+    // byte data type is used to store whole numbers
+    // byte data type is used to store positive and negative numbers
+    // byte data type is used to store numbers without decimal points
+
+    // byte data type example
+    byte b = 100;
+
+    // short data type
+    // short data type is used to store integer values
+    // short data type is used to store whole numbers
+    // short data type is used to store positive and negative numbers
+    // short data type is used to store numbers without decimal points
+
+    // short data type example
+    short s = 1000;
+
+    // long data type
+    // long data type is used to store integer values
+    // long data type is used to store whole numbers
+    // long data type is used to store positive and negative numbers
+    // long data type is used to store numbers without decimal points
+
+    // long data type example
+    long l = 1000000;
+
 }

15b8e109b3ce5571e4a04f52e2e774b03b2fe886
Baba Fakruddin D <bfakruddin@gmail.com>
2025-02-13 23:18:28 +0530

    Java - IOStreams and OOPS definitions

--- src/main/java/org/codetemplates/OOPSConcepts.java
+++ src/main/java/org/codetemplates/OOPSConcepts.java
@@ -1,4 +1,37 @@
 package org.codetemplates;
 
 public class OOPSConcepts {
+
+    public static void main(String[] args) {
+        // Inheritance
+        // Inheritance is a mechanism in which one object acquires all the properties and behaviors of a parent object.
+        // The idea behind inheritance in Java is that you can create new classes that are built upon existing classes.
+        // When you inherit from an existing class, you can reuse methods and fields of the parent class.
+        // Inheritance represents the IS-A relationship which is also known as a parent-child relationship.
+        // Inheritance is a mechanism in which one object acquires all the properties and behaviors of a parent object.
+
+        // Encapsulation
+        // Encapsulation is a mechanism of wrapping the data (variables) and code acting on the data (methods) together as a single unit.
+        // Encapsulation is used to hide the values or state of a structured data object inside a class, preventing
+        // unauthorized parties' direct access to them.
+        // Encapsulation is a technique used to protect the data in an object from being modified by external entities.
+        // Encapsulation is used to restrict access to the data in an object and only allow access through the object's methods.
+        // Encapsulation is used to hide the implementation details of an object and only expose the necessary information to the outside world.
+
+        // Polymorphism
+        // Polymorphism is a feature of object-oriented programming that allows objects to be treated as instances of their parent class.
+        // Polymorphism allows you to define methods in a parent class that can be overridden by child classes.
+        // Polymorphism allows you to define methods in a parent class that can be implemented differently by child classes.
+        // Polymorphism allows you to define methods in a parent class that can be called with objects of child classes.
+        // Polymorphism allows you to define methods in a parent class that can be called with objects of different classes.
+
+        // Abstraction
+        // Abstraction is a mechanism of hiding the implementation details of a class and only exposing the necessary information to the outside world.
+        // Abstraction is used to define the behavior of an object without revealing the details of how it is implemented.
+        // Abstraction is used to define the interface of an object without revealing the implementation details.
+        // Abstraction is used to define the properties and methods of an object without revealing the internal details of how they work.
+        // Abstraction is used to define the behavior of an object without revealing the details of how it is implemented.
+
+    }
+
 }

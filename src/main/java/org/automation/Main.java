114f16895afc0cb9c2af9ea89e55d28bd73ffc15
Baba Fakruddin D <bfakruddin@gmail.com>
2025-02-13 07:05:05 +0530

    Java - class, packages, main

--- src/main/java/org/automation/Main.java
+++ src/main/java/org/automation/Main.java
@@ -5,12 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
 //            System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
--- src/main/java/org/javapractice/HelloWorld.java
+++ src/main/java/org/javapractice/HelloWorld.java
@@ -1,7 +1,104 @@
 package org.javapractice;
 
+
+// Every Java file should have a class with the same name as the file
+// The class with the main method is the entry point of the program
+// The main method is the starting point of the program
+// The main method is the first method to be executed
+// The main method is the first method to be executed when the program is run
+
+// Class is a blueprint for objects
+// Class is a template for objects
+// Class name should start with an upper case letter
+// Class name should be a noun
+// Class name and file name should be the same
+// Class name should be meaningful
+
+// Every Java program should have at least one class
+// Every Java program should have at least one main method
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        switch (args[0]){
+            case "Chrome":
+                System.out.println("Our program started with Chrome");
+                break;
+            case "Firefox":
+                System.out.println("Our program started with Firefox");
+                System.setProperty("webdriver.gecko.driver", "/Users/baba/Documents/GitHub/2025-Selenium/seleniumtraining/src/main/resources/drivers/geckodriver");
+                WebDriver driver = new FirefoxDriver();
+                break;
+            case "Safari":
+                System.out.println("Our program started with Safari");
+                break;
+            default:
+                System.out.println("Our program started with Selenium");
+        }
+
         System.out.println("Hello World!");
+        System.out.println(testing());
+    }
+
+    public static boolean testing(){
+        Vehicle car = new Vehicle();
+        car.name = "Audi";
+        car.color = "Black";
+        car.model = "A4";
+        car.brand = "Audi";
+        car.type = "Sedan";
+        car.price = 50000;
+
+        System.out.println("Car details: " + car);
+        return false;
     }
 }
+
+/**
+// packages in small characters
+// classes in camel casing starting with upper case
+// methods in camel casing starting with lower case
+// variables in camel casing starting with lower case
+// constants in all capital letters with underscores
+*/
+
+/*
+Things -->
+LivingThings -->
+Trees --> CoconutTree
+Animals -->
+Insects --> Butterfly
+Fish --> Shark
+Birds --> Eagle
+Mammals --> Human
+Human --> Male or Female
+Male --> American, Indian, Chinese
+
+--> Non_LivingThings -->
+Vehicle --> Car
+Furniture --> Chair*/
+
+class Vehicle{
+    String name;
+    String color;
+    String model;
+    String brand;
+    String type;
+    int price;
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+

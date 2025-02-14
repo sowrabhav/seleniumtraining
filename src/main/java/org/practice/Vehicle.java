9a84f16979f008e6acd95ca74d0e58fcc5540369
sowrabhav <sowphotos@gmail.com>
2025-02-14 17:57:15 -0600

    update new libraries

---
+++ src/main/java/org/practice/Vehicle.java
@@ -0,0 +1,11 @@
+package org.practice;
+
+public class Vehicle {
+    String color;
+    String model;
+    String brand;
+    int value;
+    void display(){
+        System.out.println( brand+" "+ model +" " +color );
+    }
+}

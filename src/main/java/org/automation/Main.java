1833445ceeca82dd44ed02ed78c4cae06d0c2138
Baba Fakruddin D <bfakruddin@gmail.com>
2025-02-28 07:11:52 +0530

    28/Feb/2024 - Strings, StringBuilder, StringBuffer

--- src/main/java/org/automation/Main.java
+++ src/main/java/org/automation/Main.java
@@ -9,13 +9,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-
-
 //            System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
         System.setProperty("webdriver.gecko.driver", "/Users/baba/Documents/GitHub/2025-Selenium/seleniumtraining/src/main/resources/drivers/geckodriver");
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.google.com");
-        WebElement element = driver.findElement(By.name("q"));
+
+        // set browser properties
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().deleteCookieNamed("sessionKey");
+
+        driver.get("https://www.google.com"); // initialize your application
+        WebElement element = driver.findElement(By.name("q")); // find the element
+
         element.sendKeys("Tajmahal");
 
 //        element.submit();
--- src/main/java/org/codetemplates/Strings.java
+++ src/main/java/org/codetemplates/Strings.java
@@ -7,20 +7,34 @@ public class Strings {
         String str3 = new String("Hello");
         String str4 = new String("Hello");
 
-        System.out.println(str1 == str2);
-        System.out.println(str1 == str3);
-        System.out.println(str3 == str4);
-        System.out.println(str1.equals(str2));
-        System.out.println(str1.equals(str3));
-        System.out.println(str3.equals(str4));
+//        System.out.println(str1 == str2);
+//        System.out.println(str1 == str3);
+//        System.out.println(str3 == str4);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.equals(str3));
+//        System.out.println(str3.equals(str4));
 
-        StringBuffer sb1 = new StringBuffer("Hello");
+        String s = "Ea";
+        String s1 = "Fb";
+
+        System.out.println(s.hashCode() + " " + s1.hashCode());
+//        System.out.println(s1.compareTo(s));
+//        System.out.println(s1.equals(s));
+//        System.out.println(20==10);
+
+
+        StringBuffer sb1 = new StringBuffer("Hello ");
+        // String buffer is mutable
+        // Stringbuffer is synchronized
+        // String buffer is thread safe
+        sb1.append("USA");
+
 
         StringBuilder sb2 = new StringBuilder("Hello");
-
+        // String builder is mutable
+        // Stringbuilder is not synchronized
+        // String builder is not thread safe
 
     }
-
-
 
 }

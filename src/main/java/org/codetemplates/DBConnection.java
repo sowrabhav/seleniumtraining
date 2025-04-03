edbae3cfcd4cfc7a083b6ebb59e4707ecdebf9a0
Baba Fakruddin D <bfakruddin@gmail.com>
2025-04-03 07:13:44 +0530

    03/04/25 - Database connection with Java

--- src/main/java/org/codetemplates/DBConnection.java
+++ src/main/java/org/codetemplates/DBConnection.java
@@ -4,6 +4,9 @@ package org.codetemplates;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * DB Connection using Mysql
@@ -11,11 +14,16 @@ import java.sql.*;
 public class DBConnection {
     public static void main(String[] args) throws SQLException {
         Connection con = null;
+
+        // connect to the database
         MySqlConnect mySqlConnect = new MySqlConnect();
         con = mySqlConnect.connect();
         System.out.println("Connected to the database");
 
+        // prepate a statement (query)
         PreparedStatement statement = con.prepareStatement("select * from T_Google");
+
+        // execute a query and get the result set
         ResultSet resultSet = statement.executeQuery();
 
         for (;resultSet.next();) {
@@ -39,19 +47,18 @@ public class DBConnection {
      * */
     public static Connection connectToDatabase() throws SQLException {
 
-        // approach 1
+        // approach 1 (jdbc:dbserver://hostNameOrIP:portNumber/dbName)
         String url = "jdbc:mysql://localhost:3306/sys";
         String username = "root";
         String password = "baba2222";
         Connection connection = DriverManager.getConnection(url, username, password);
 
         // approach 2
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL("jdbc:mysql://localhost:3306/sys");
-        dataSource.setUser("root");
-        dataSource.setPassword("baba2222");
-
-        Connection connection1 = dataSource.getConnection();
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setURL("jdbc:mysql://localhost:3306/sys");
+//        dataSource.setUser("root");
+//        dataSource.setPassword("baba2222");
+//        Connection connection1 = dataSource.getConnection();
 
 
         return connection;
